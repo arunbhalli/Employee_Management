b636a7b4fdf54cd2a41c56da23d496f7e8bfd571
arunbhalli <tomarbhalli.com>
2021-04-12 13:14:16 +0200

    cypress test error

--- cypress/integration/displayEmployeeList.feature.js
+++ cypress/integration/displayEmployeeList.feature.js
@@ -1,8 +1,14 @@
+
 describe("Display list of employees", () => {
   beforeEach(() => {
-    cy.visit("/");
+    cy.visit("https://localhost:3000");
   });
   it("display a header", () => {
-    cy.get("#header").should("contain", "Employee List");
+    cy.get('#header').should("contain", "Employee List");
   });
+  // it('displays a list with 6 items', () => {
+  //   cy.get("[data-cy=employee-list]").within(() => {
+  //     cy.get("[data-cy=employee-item]").should('have.length',6)
+  //   })
+  // })
 });
--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div >
+    <h1 id="header">Employee List </h1>
     </div>
+
   );
 }
 
