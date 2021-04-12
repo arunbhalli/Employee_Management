525140f56850ba9e7b2dcc68c4c241e13c725f5e
arunbhalli <tomarbhalli.com>
2021-04-12 14:21:31 +0200

    rendering a list in react is done

--- cypress/integration/displayEmployeeList.feature.js
+++ cypress/integration/displayEmployeeList.feature.js
@@ -1,14 +1,24 @@
-
 describe("Display list of employees", () => {
   beforeEach(() => {
     cy.visit("/");
   });
+
   it("display a header", () => {
-    cy.get('#header').should("contain", "Employee List");
+    cy.get("[data-cy=header]").should("contain", "Employee List");
   });
-  it('displays a list with 6 items', () => {
+
+  it("displays a list with 6 items", () => {
     cy.get("[data-cy=employee-list]").within(() => {
-      cy.get("[data-cy=employee-item]").should('have.length',6)
-    })
-  })
+      cy.get("[data-cy=employee-item]").should("have.length", 6);
+    });
+  });
+  
+  it("the list items display the expected content", () => {
+    cy.get("[data-cy=employee-list]").within(() => {
+      cy.get("[data-cy=employee-item]")
+      .first()
+      .find(".name")
+      .should("contain", "George Bluth");
+    });
+  });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import EmployeeList from './component/EmployeeList.jsx'
 import React, { Component} from "react"
 
 class  App extends Component {
 render() {
   return(
     <div >
-    <h1 id="header">Employee List </h1>
+    <h1 data-cy="header">Employee List </h1>
+    <EmployeeList />
     </div>
   )
 }
