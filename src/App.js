2c8a1badf2ebf1d3e7734e46d9b5024ce30ce9cf
arunbhalli <tomarbhalli.com>
2021-04-12 13:21:05 +0200

    componenet and react import added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
-import './App.css';
+import React, { Component} from "react"
 
-function App() {
-  return (
+class  App extends Component {
+render() {
+  return(
     <div >
     <h1 id="header">Employee List </h1>
     </div>
-
-  );
+  )
+}
 }
 
 export default App;
